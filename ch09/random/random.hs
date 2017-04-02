e846adbd7c9410c61152c1870c75d162fe4dd120
Yoshitomo Minami <minami530@gmail.com>
2017-04-02 21:15:13 +0900

    Read up to chapter 9

---
+++ ch09/random/random.hs
@@ -0,0 +1,22 @@
+import System.Random
+import Control.Monad(when)
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in  (firstCoin, secondCoin, thirdCoin)
+
+main = do
+    gen <- getStdGen
+    let (randNumber, _) = randomR (1,10) gen :: (Int, StdGen)
+    putStrLn "Which number in the range from 1 to 10 am I thinking of? "
+    numberString <- getLine
+    when (not $ null numberString) $ do
+        let number = read numberString
+        if randNumber == number
+            then putStrLn "You are correct!"
+            else putStrLn $ "Sorry, it was " ++ show randNumber
+        newStdGen
+        main
